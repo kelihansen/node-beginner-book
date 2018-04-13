57adbd9309272fae1c333f75e8f7ab1365bd97ba
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-12 21:19:53 -0700

    server, router, and request handlers connected

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = require('./server');
 const router = require('./router');
-const requestHandlers = ('./request-handlers');
+const requestHandlers = require('./request-handlers');
 
 const handle = {};
 handle['/'] = requestHandlers.start;
 handle['/start'] = requestHandlers.start;
 handle['/upload'] = requestHandlers.upload;
 
-server.start(router.route);
+server.start(router.route, handle);
