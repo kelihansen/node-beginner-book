7b748b6a5fe40f94915e771df1a9d4886b391909
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-13 09:29:15 -0700

    how not to do it

--- request-handlers.js
+++ request-handlers.js
@@ -1,9 +1,18 @@
+const exec = require('child_process').exec;
+
 function start() {
     console.log('Request handler "start" was called.');
+    let content = 'empty';
+
+    exec('ls -lah', (error, stdout, stderr) => {
+        content = stdout;
+    });
+    return content;
 }
 
 function upload() {
     console.log('Request handler "upload" was called.');
+    return 'Hello Upload';
 }
 
 exports.start = start;
